dcc263e1aef45d46fad745ac3137862fac2896e1
DosuSolomon <dosusulaimon.s@gmail.com>
2020-06-06 23:18:06 +0100

    'module2_solution'

---
+++ app.js
@@ -0,0 +1,55 @@
+(function () {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ToBuyController', ToBuyController)
+        .controller('AlreadyBoughtController', AlreadyBoughtController)
+        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuyList = this;
+
+        toBuyList.items = ShoppingListCheckOffService.getToBuyItems();
+
+        toBuyList.buyItem = function (itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        };
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var alreadyBougthList = this;
+
+        alreadyBougthList.items = ShoppingListCheckOffService.getAlreadyBoughtItems();
+    }
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+        var toBuyItems = [
+            { name: "Shawamma", quantity: 4 },
+            { name: "Pizza", quantity: 10 },
+            { name: "Noodles", quantity: 25 },
+            { name: "Apple", quantity: 1 },
+            { name: "Peneaples", quantity: 6 }
+        ];
+        var alreadyBoughtItems = [];
+
+        service.buyItem = function (itemIndex) {
+            var item = toBuyItems[itemIndex];
+
+            alreadyBoughtItems.push(item);
+            toBuyItems.splice(itemIndex, 1);
+        };
+
+        service.getToBuyItems = function () {
+            return toBuyItems;
+        };
+
+        service.getAlreadyBoughtItems = function () {
+            return alreadyBoughtItems;
+        };
+    }
+})();
